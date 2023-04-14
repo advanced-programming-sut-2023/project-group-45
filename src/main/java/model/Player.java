75a6e866d1b98c7511758988ad194fadbe07412d
Alireza Samimi <alirezasamimi100@yahoo.com>
2023-04-14 11:34:38 +0330

    Add Game, Trade and Market classes

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,17 +1,20 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Player {
     private final User user;
     private int happiness, fear;
     private final HashMap<String, Integer> resources;
+    private final ArrayList<TradeRequest> tradeRequests;
 
     Player(User user) {
         this.user = user;
         happiness = 0;
         fear = 0;
         resources = new HashMap<>();
+        tradeRequests = new ArrayList<>();
     }
 
     public User getUser() {
@@ -36,5 +39,9 @@ public class Player {
 
     public HashMap<String, Integer> getResources() {
         return resources;
+    }
+
+    public ArrayList<TradeRequest> getTradeRequests() {
+        return tradeRequests;
     }
 }
--- src/main/java/model/Tile.java
+++ src/main/java/model/Tile.java
@@ -1,6 +1,31 @@
 package model;
 
-import java.util.ArrayList;
-
 public class Tile {
+    private String type;
+    private Building building;
+
+    public Tile() {
+        this.type = "plain";
+        this.building = null;
+    }
+    public Tile(String type) {
+        this.type = type;
+        this.building = null;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Building getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(Building building) {
+        this.building = building;
+    }
 }

1da1feb2cede13d7fd87aad827cd7ea90e77ccd4
Alireza Kaviani <alireza.kaviani.2005@gmail.com>
2023-04-14 21:44:03 +0330

    Add MapMenu

---
+++ src/main/java/view/MapMenu.java
@@ -0,0 +1,22 @@
+package view;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class MapMenu extends Menu {
+    private int x, y;
+
+    public MapMenu(Scanner scanner, int x, int y){
+        super(scanner);
+        this.x = x;
+        this.y = y;
+    }
+
+    private void move(HashMap<String, String> input){
+        return;
+    }
+
+    private void showDetails(HashMap<String, String> input){
+        return;
+    }
+}

0dadd8d241aaee6121201da47fee09e3e8fd3704
Alireza Kaviani <alireza.kaviani.2005@gmail.com>
2023-04-14 23:00:22 +0330

    Add utils

---
+++ src/main/java/view/Utils.java
@@ -0,0 +1,13 @@
+package view;
+
+import java.util.HashMap;
+
+public abstract class Utils {
+    public static HashMap<String, String> parseInput(String input){
+        return null;
+    }
+
+    public static void Print(){
+        return;
+    }
+}

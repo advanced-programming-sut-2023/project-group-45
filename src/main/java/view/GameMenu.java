dccec3d4fad08cb46b8fd5124e629e6cfc127700
Alireza Kaviani <alireza.kaviani.2005@gmail.com>
2023-04-14 22:51:30 +0330

    Add Economy

--- src/main/java/view/GameMenu.java
+++ src/main/java/view/GameMenu.java
@@ -3,89 +3,122 @@ package view;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class GameMenu extends Menu{
-    public GameMenu(Scanner scanner){
+public class GameMenu extends Menu {
+    public GameMenu(Scanner scanner) {
         super(scanner);
         // TODO: add command
     }
 
-    private void changeUsername(HashMap<String, String> input){
+    private void changeUsername(HashMap<String, String> input) {
         return;
     }
 
-    private void changeNickname(HashMap<String, String> input){
+    private void changeNickname(HashMap<String, String> input) {
         return;
     }
 
-    private void changePassword(HashMap<String, String> input){
+    private void changePassword(HashMap<String, String> input) {
         return;
     }
 
-    private void changeEmail(HashMap<String, String> input){
+    private void changeEmail(HashMap<String, String> input) {
         return;
     }
 
-    private void changeSlogan(HashMap<String, String> input){
+    private void changeSlogan(HashMap<String, String> input) {
         return;
     }
 
-    private void displayHighScore(HashMap<String, String> input){
+    private void displayProfile(HashMap<String, String> input) {
         return;
     }
 
-    private void displayRank(HashMap<String, String> input){
+    private void showMap(HashMap<String, String> input) {
         return;
     }
 
-    private void displaySlogan(HashMap<String, String> input){
+    private void showPopularityFactors(HashMap<String, String> input) {
         return;
     }
 
-    private void displayProfile(HashMap<String, String> input){
+    private void showPopularity(HashMap<String, String> input) {
         return;
     }
 
-    private void showMap(HashMap<String, String> input){
+    private void showFoodList(HashMap<String, String> input) {
         return;
     }
 
-    private void showPopularityFactors(HashMap<String, String> input){
+    private void foodRate(HashMap<String, String> input) {
         return;
     }
 
-    private void showPopularity(HashMap<String, String> input){
+    private void taxRate(HashMap<String, String> input) {
         return;
     }
 
-    private void showFoodList(HashMap<String, String> input){
+    private void fearRate(HashMap<String, String> input) {
         return;
     }
 
-    private void foodRate(HashMap<String, String> input){
+    private void dropBuilding(HashMap<String, String> input) {
         return;
     }
 
-    private void taxRate(HashMap<String, String> input){
+    private void selectBuilding(HashMap<String, String> input) {
         return;
     }
 
-    private void fearRate(HashMap<String, String> input){
+    private void dropUnit(HashMap<String, String> input) {
         return;
     }
 
-    private void dropBuilding(HashMap<String, String> input){
+    private void selectUnit(HashMap<String, String> input) {
         return;
     }
 
-    private void selectBuilding(HashMap<String, String> input){
+    private void setTileTexture(HashMap<String, String> input) {
         return;
     }
 
-    private void dropUnit(HashMap<String, String> input){
+    private void clearTile(HashMap<String, String> input) {
         return;
     }
 
-    private void selectUnit (HashMap<String, String> input){
+    private void dropRock(HashMap<String, String> input) {
         return;
     }
+
+    private void dropTree(HashMap<String, String> input) {
+        return;
+    }
+
+    private void trade(HashMap<String, String> input) {
+        return;
+    }
+
+    private void tradeList(HashMap<String, String> input) {
+        return;
+    }
+
+    private void tradeAccept(HashMap<String, String> input) {
+        return;
+    }
+
+    private void tradeHistory(HashMap<String, String> input) {
+        return;
+    }
+
+    private void showPriceList(HashMap<String, String> input) {
+        return;
+    }
+
+    private void butItem(HashMap<String, String> input) {
+        return;
+    }
+
+    private void sellItem(HashMap<String, String> input) {
+        return;
+    }
+
 }

cbcdabf90634aa87f046d1cb5b902dc5aa89f7ec
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-04-25 10:36:48 +0330

    Implement base menu

--- src/main/java/stronghold/view/Menu.java
+++ src/main/java/stronghold/view/Menu.java
@@ -4,21 +4,44 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.function.Consumer;
-import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
-@Data
+@RequiredArgsConstructor
 public abstract class Menu {
 
     private final Scanner scanner;
     private final Map<String, Consumer<Map<String, String>>> commands = new HashMap<>();
 
     protected final void addCommand(String commandName,
-            Consumer<HashMap<String, String>> function) {
+            Consumer<Map<String, String>> function) {
+        commands.put(commandName, function);
     }
 
-    private void processCommands(String input) {
+    private void processCommand(CommandLineParser parser) {
+        String commandName = parser.getCommand();
+        if (commands.containsKey(commandName)) {
+            commands.get(commandName).accept(parser.getOptions());
+        } else {
+            System.out.println("Unknown command: " + commandName);
+        }
     }
 
-    public void runMenu() {
+    private void processLine(String line) {
+        try {
+            Tokenizer tokenizer = new Tokenizer(line);
+            CommandLineParser parser = new CommandLineParser(tokenizer.getTokens());
+            processCommand(parser);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Illegal input: " + e.getMessage());
+        }
+    }
+
+    public void run() {
+        while (true) {
+            String line = scanner.nextLine().trim();
+            if (line.equals("back"))
+                return;
+            processLine(line);
+        }
     }
 }

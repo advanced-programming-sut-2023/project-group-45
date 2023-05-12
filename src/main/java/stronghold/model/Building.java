15ee6a02e26040b51c301cedbb117be3e0364723
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-05-12 20:15:26 +0330

    Add some of religion/happiness related buildings

--- src/main/java/stronghold/model/Building.java
+++ src/main/java/stronghold/model/Building.java
@@ -19,11 +19,13 @@ public class Building implements Serializable {
     private final Map<String, Integer> supply;
     private int hitPoints;
     private int labors = 0;
+    private final int religionFactor;
+    private final int happinessFactor;
 
     @Builder(toBuilder = true)
     public Building(String type, int maxHitPoints, boolean isHollow, Player owner,
             IntPair position, int maxLabors, Map<String, Integer> consume,
-            Map<String, Integer> supply) {
+            Map<String, Integer> supply, int religionFactor, int happinessFactor) {
         this.type = type;
         this.maxHitPoints = maxHitPoints;
         this.hitPoints = maxHitPoints;
@@ -33,5 +35,7 @@ public class Building implements Serializable {
         this.maxLabors = maxLabors;
         this.consume = consume;
         this.supply = supply;
+        this.religionFactor = religionFactor;
+        this.happinessFactor = happinessFactor;
     }
 }

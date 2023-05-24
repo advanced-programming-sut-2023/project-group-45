e05853282d784026eb7cc95c08e9f9373dd17cca
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-05-24 22:14:20 +0330

    Fix phase 1

--- src/main/java/stronghold/model/TradeRequest.java
+++ src/main/java/stronghold/model/TradeRequest.java
@@ -4,12 +4,15 @@ import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
 @AllArgsConstructor
 public class TradeRequest implements Serializable {
 
+    @ToString.Exclude
     private Player sender;
+    @ToString.Exclude
     private Player receiver;
     private String item;
     private int amount;

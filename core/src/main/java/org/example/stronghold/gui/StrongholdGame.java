c5abbb2c9946d1ed48f163792142979a3e33e56e
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-06-29 10:48:56 +0330

    Get GameData from operator

--- core/src/main/java/org/example/stronghold/gui/StrongholdGame.java
+++ core/src/main/java/org/example/stronghold/gui/StrongholdGame.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import org.example.stronghold.gui.sections.MapScreen;
 import org.example.stronghold.model.Database;
@@ -14,6 +15,7 @@ import org.example.stronghold.model.template.BuildingTemplate;
 import org.example.stronghold.model.template.GameMapTemplate;
 import org.example.stronghold.model.template.TemplateDatabase;
 import org.example.stronghold.model.template.UnitTemplate;
+import org.example.stronghold.operator.OperatorException;
 import org.example.stronghold.operator.Operators;
 
 public class StrongholdGame extends Game {
@@ -42,12 +44,17 @@ public class StrongholdGame extends Game {
         craftacularSkin = new Skin(Gdx.files.internal("craftacular/craftacular-ui.json"));
         assetLoader.loadAll();
 //        setScreen(new LoginScreen(this));
-        List<User> users = database.getUsers().stream().limit(2).toList();
-        GameMapTemplate gameMapTemplate = templateDatabase.getGameMapTemplates().get("test");
-        UnitTemplate lordTemplate = templateDatabase.getUnitTemplates().get("Lord");
-        BuildingTemplate buildingTemplate = templateDatabase.getBuildingTemplates().get("Base");
-        GameData gameData = new GameData(users, gameMapTemplate, lordTemplate, buildingTemplate);
-        setScreen(new MapScreen(this, gameData));
+        try {
+            List<User> users = database.getUsers().stream().limit(2).toList();
+            GameMapTemplate gameMapTemplate = templateDatabase.getGameMapTemplates().get("test");
+            GameData gameData = Operators.game.startGame(new HashMap<>() {{
+                put("map", gameMapTemplate);
+                put("users", users);
+            }});
+            setScreen(new MapScreen(this, gameData));
+        } catch (OperatorException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override

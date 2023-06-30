2def51e27824b88783baeccc54930bc3cf3ca7b3
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-06-30 11:29:23 +0330

    Implement mode buttons

--- core/src/main/java/org/example/stronghold/gui/panels/UnitPanel.java
+++ core/src/main/java/org/example/stronghold/gui/panels/UnitPanel.java
@@ -1,17 +1,20 @@
 package org.example.stronghold.gui.panels;
 
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import org.example.stronghold.context.IntPair;
+import org.example.stronghold.gui.SimpleChangeListener;
 import org.example.stronghold.gui.components.ControlPanel;
 import org.example.stronghold.gui.components.Panel;
 import org.example.stronghold.model.Unit;
+import org.example.stronghold.operator.OperatorException;
+import org.example.stronghold.operator.Operators;
 
 public class UnitPanel extends Panel {
 
@@ -26,10 +29,11 @@ public class UnitPanel extends Panel {
     }
 
     private void toggleUnit(Unit unit) {
-        if (units.contains(unit))
+        if (units.contains(unit)) {
             units.remove(unit);
-        else
+        } else {
             units.add(unit);
+        }
     }
 
     public void toggleCell(int col, int row) {
@@ -43,10 +47,17 @@ public class UnitPanel extends Panel {
         return new TextureRegionDrawable(game.assetLoader.getTexture("banners/" + banner + ".png"));
     }
 
-    private ImageButton getButton(String banner) {
+    private ImageButton newButton(String banner, Runnable runnable) {
         ImageButton btn = new ImageButton(getBanner(banner));
         btn.setSkin(game.skin);
+        if (runnable != null) {
+            btn.addListener(new SimpleChangeListener(runnable));
+        }
         return btn;
+    }
+
+    private ImageButton newModeButton(String mode) {
+        return newButton(mode, () -> this.setMode(mode));
     }
 
     private void create() {
@@ -54,12 +65,12 @@ public class UnitPanel extends Panel {
         info.setWrap(true);
         add(info).growX().row();
 
-        defensiveBtn = getButton("defensive");
-        offensiveBtn = getButton("offensive");
-        standingBtn = getButton("standing");
-        moveBtn = getButton("move");
-        disbandBtn = getButton("disband");
-        attackBtn = getButton("attack");
+        defensiveBtn = newModeButton("defensive");
+        offensiveBtn = newModeButton("offensive");
+        standingBtn = newModeButton("standing");
+        moveBtn = newButton("move", null);
+        disbandBtn = newButton("disband", null);
+        attackBtn = newButton("attack", null);
 
         banners = new Table(game.skin);
         banners.add(defensiveBtn, standingBtn, offensiveBtn, disbandBtn, moveBtn, attackBtn);
@@ -74,8 +85,9 @@ public class UnitPanel extends Panel {
         StringBuilder stringBuilder = new StringBuilder();
         boolean first = true;
         for (Unit unit : units) {
-            if (!first)
+            if (!first) {
                 stringBuilder.append(" - ");
+            }
             first = false;
             stringBuilder.append(String.format("%s %d",
                 unit.getType(),
@@ -83,5 +95,17 @@ public class UnitPanel extends Panel {
             ));
         }
         info.setText(stringBuilder);
+    }
+
+    public void setMode(String mode) {
+        try {
+            Operators.game.setUnitMode(new HashMap<>() {{
+                put("units", units);
+                put("mode", mode);
+            }});
+            controlPanel.popup.success("Changed mode to " + mode);
+        } catch (OperatorException e) {
+            controlPanel.popup.error(e.getMessage());
+        }
     }
 }

c8e657275c6113df8003dd24b54541c7968da528
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-06-30 11:21:00 +0330

    Add buttons for banners

--- core/src/main/java/org/example/stronghold/gui/panels/UnitPanel.java
+++ core/src/main/java/org/example/stronghold/gui/panels/UnitPanel.java
@@ -1,6 +1,11 @@
 package org.example.stronghold.gui.panels;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 import java.util.ArrayList;
 import java.util.List;
 import org.example.stronghold.context.IntPair;
@@ -12,6 +17,8 @@ public class UnitPanel extends Panel {
 
     public final List<Unit> units = new ArrayList<>();
     Label info;
+    Table banners;
+    ImageButton defensiveBtn, offensiveBtn, standingBtn, moveBtn, disbandBtn, attackBtn;
 
     public UnitPanel(ControlPanel controlPanel) {
         super(controlPanel);
@@ -32,10 +39,31 @@ public class UnitPanel extends Panel {
         updateInfo();
     }
 
+    private TextureRegionDrawable getBanner(String banner) {
+        return new TextureRegionDrawable(game.assetLoader.getTexture("banners/" + banner + ".png"));
+    }
+
+    private ImageButton getButton(String banner) {
+        ImageButton btn = new ImageButton(getBanner(banner));
+        btn.setSkin(game.skin);
+        return btn;
+    }
+
     private void create() {
         info = new Label("", game.skin);
         info.setWrap(true);
         add(info).growX().row();
+
+        defensiveBtn = getButton("defensive");
+        offensiveBtn = getButton("offensive");
+        standingBtn = getButton("standing");
+        moveBtn = getButton("move");
+        disbandBtn = getButton("disband");
+        attackBtn = getButton("attack");
+
+        banners = new Table(game.skin);
+        banners.add(defensiveBtn, standingBtn, offensiveBtn, disbandBtn, moveBtn, attackBtn);
+        add(banners).align(Align.left).row();
     }
 
     private void updateInfo() {

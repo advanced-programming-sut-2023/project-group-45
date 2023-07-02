3d9b5657636aeab11efcfb487ad54c3a092a37d2
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-07-02 10:22:11 +0330

    Prepare for returning object

--- core/src/main/java/org/example/stronghold/server/RequestHandler.java
+++ core/src/main/java/org/example/stronghold/server/RequestHandler.java
@@ -1,5 +1,6 @@
 package org.example.stronghold.server;
 
+import static com.google.common.base.Preconditions.checkNotNull;
 import static org.example.stronghold.context.MapUtils.*;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -8,6 +9,11 @@ import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.Data;
+import org.example.stronghold.model.Building;
+import org.example.stronghold.model.GameData;
+import org.example.stronghold.model.Player;
+import org.example.stronghold.model.Unit;
+import org.example.stronghold.operator.Operators;
 
 @Data
 public class RequestHandler implements Runnable {
@@ -33,14 +39,18 @@ public class RequestHandler implements Runnable {
         try {
             Map<String, Object> request = (Map<String, Object>) input.readObject();
             String what = getReqString(request, "what");
-            System.out.println(request);
-            sendResponse(new HashMap<>() {{
-                put("what", "response");
-                put("data", "ok");
-            }});
-        } catch (IllegalArgumentException e) {
+            if (what.equals("object")) {
+                handleObject(request);
+                return;
+            }
+            sendError("Unknown request: " + what);
+        } catch (RuntimeException e) {
             sendError(e.getMessage());
         }
+    }
+
+    private void handleObject(Map<String, Object> req) throws IOException, ClassNotFoundException {
+        sendError("not implemented");
     }
 
     private void sendResponse(Map<String, Object> response) throws IOException {
--- core/src/main/java/org/example/stronghold/server/Server.java
+++ core/src/main/java/org/example/stronghold/server/Server.java
@@ -1,13 +1,28 @@
 package org.example.stronghold.server;
 
+import java.io.File;
+import java.io.IOException;
 import lombok.Data;
+import org.example.stronghold.model.Database;
+import org.example.stronghold.model.template.TemplateDatabase;
+import org.example.stronghold.operator.Operators;
 
 @Data
 public class Server {
     public static final int PORT = 2222;
+    private static final File databaseFile = new File("./assets/data/database.ser");
+    private static final File templateDatabaseRoot = new File("./assets/data");
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Database database = Database.fromFileOrDefault(databaseFile);
+        TemplateDatabase templateDatabase = new TemplateDatabase();
+        templateDatabase.updateFromPath(templateDatabaseRoot);
+        Operators.initDatabase(database, templateDatabase);
+
         ConnectionHandler connectionHandler = new ConnectionHandler(PORT);
         connectionHandler.run();
+
+        database.toFile(databaseFile);
+        templateDatabase.saveToPath(templateDatabaseRoot);
     }
 }

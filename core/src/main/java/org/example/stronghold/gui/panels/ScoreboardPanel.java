73fd9155a5af8fd0df90732f5d29270f3c6e4100
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-07-05 20:14:39 +0330

    Update on act

--- core/src/main/java/org/example/stronghold/gui/panels/ScoreboardPanel.java
+++ core/src/main/java/org/example/stronghold/gui/panels/ScoreboardPanel.java
@@ -5,49 +5,50 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Align;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.TimeZone;
 import org.example.stronghold.gui.components.ControlPanel;
 import org.example.stronghold.gui.components.Panel;
 import org.example.stronghold.model.User;
 
-import java.text.SimpleDateFormat;
-import java.util.*;
-
 public class ScoreboardPanel extends Panel {
 
     private Table table;
-    public Thread updateThread;
+    private int turn = 0;
 
     public ScoreboardPanel(ControlPanel controlPanel) {
         super(controlPanel);
         create();
     }
 
-    private void addLabel(String value){
+    private void addLabel(String value) {
         Label label = new Label(value, game.skin);
         label.setAlignment(Align.center);
         table.add(label).growX();
     }
 
-    private void create(){
+    private void create() {
         table = new Table(game.skin);
         add(table).grow();
-        updateThread = new Thread(() -> {
-            for (int i = 0; i < 12; i++){
-                table.reset();
-                updateScoreboard();
-                try {
-                    Thread.sleep(10 * 1000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-//        updateThread.start();
-        updateScoreboard();
     }
 
-    private String getLastVisit(User user){
-        if(user.isOnline()){
+    @Override
+    public void act(float delta) {
+        if (++turn == 60) {
+            table.reset();
+            updateScoreboard();
+            turn = 0;
+        }
+        super.act(delta);
+    }
+
+    private String getLastVisit(User user) {
+        if (user.isOnline()) {
             return "Online now";
         }
         long lastVisit = user.getLastVisit();
@@ -57,7 +58,7 @@ public class ScoreboardPanel extends Panel {
         return sdf.format(date);
     }
 
-    private void updateScoreboard(){
+    private void updateScoreboard() {
         try {
             table.align(Align.center);
             addLabel("Avatar");
@@ -65,10 +66,11 @@ public class ScoreboardPanel extends Panel {
             addLabel("Score");
             addLabel("Last Visit");
             table.row();
-            List<User> users = new ArrayList<>((List<User>) game.conn.sendOperatorRequest("auth", "getUsers",
-                new HashMap<>()));
+            List<User> users = new ArrayList<>(
+                (List<User>) game.conn.sendOperatorRequest("auth", "getUsers",
+                    new HashMap<>()));
             users.sort(Comparator.comparing(User::getScore).reversed());
-            for(User user : users){
+            for (User user : users) {
                 table.add(new Image(new Texture(user.getAvatarFileHandle()))).growX();
                 addLabel(user.getUsername());
                 addLabel(String.valueOf(user.getScore()));
@@ -76,7 +78,8 @@ public class ScoreboardPanel extends Panel {
                 table.row();
             }
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            controlPanel.popup.error(e.getMessage());
+            e.printStackTrace();
         }
     }
 }

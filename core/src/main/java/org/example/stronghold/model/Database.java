a079e9f368734bae0e28224f3466d9649efa0308
Parsa Alizadeh <parsa.alizadeh1@gmail.com>
2023-07-05 15:14:16 +0330

    Reformat sql related files

--- core/src/main/java/org/example/stronghold/model/Database.java
+++ core/src/main/java/org/example/stronghold/model/Database.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Cleanup;
